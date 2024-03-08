f844e79b02293338bc0a17e40c96c28785607ea4
Tiffany <tbcasper16@gmail.com>
2024-03-08 17:59:36 -0600

    adding closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName) {
+    alert('Welcome ' + fullName);
+}
+
+var guillaume = welcomeMessage('Guillaume');
+var alex = welcomeMessage('Alex');
+var fred = welcomeMessage('Fred');
